36e8f5f0d7a2ecce3fb94bf39d375c92f1c528f7
Benjamin Huang <coolconfucius@yahoo.com>
2016-12-06 12:29:21 -0800

    can add and display mongooses

--- server.js
+++ server.js
@@ -13,12 +13,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   our db in mongodb -- this should match the name of the db you are going to use for your project.
 mongoose.connect('mongodb://localhost/basic_mongoose');
 
-var UserSchema = new mongoose.Schema({
+// var UserSchema = new mongoose.Schema({
+//  name: String,
+//  age: Number
+// })
+// mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
+// var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+
+var MongooseSchema = new mongoose.Schema({
  name: String,
  age: Number
 })
-mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
-var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+mongoose.model('Mongoose', MongooseSchema); 
+var Mongoose = mongoose.model('Mongoose');
+
+
 
 // Use native promises
 mongoose.Promise = global.Promise;
@@ -40,37 +49,130 @@ app.set('view engine', 'ejs');
 // })
 
 // The root route -- we want to get all of the users from the database and then render the index view passing it all of the users
+// app.get('/', function(req, res) {
+  // User.find({}, function(err, users) {
+  //   // This is the method that finds all of the users from the database
+  //   // Notice how the first parameter is the options for what to find and the second is the
+  //   //   callback function that has an error (if any) and all of the users
+  //   // Keep in mind that everything you want to do AFTER you get the users from the database must
+  //   //   happen inside of this callback for it to be synchronous 
+  //   // Make sure you handle the case when there is an error, as well as the case when there is no error
+  //   console.log(users);
+  //   res.render('index', {users})
+  // })
+// })
+
+// Add User Request 
+// app.post('/users', function(req, res) {
+//   console.log("POST DATA", req.body);
+//   // This is where we would add the user from req.body to the database.
+//   // res.redirect('/');
+
+//   var user = new User({name: req.body.name, age: req.body.age});
+//   // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
+//   user.save(function(err) {
+//     // if there is an error console.log that something went wrong!
+//     if(err) {
+//       console.log('something went wrong');
+//     } else { // else console.log that we did well and then redirect to the root route
+//       console.log('successfully added a user!');
+//       res.redirect('/');
+//     }
+//   })
+// })
+
+
+
+
+
+
 app.get('/', function(req, res) {
-  User.find({}, function(err, users) {
-    // This is the method that finds all of the users from the database
-    // Notice how the first parameter is the options for what to find and the second is the
-    //   callback function that has an error (if any) and all of the users
-    // Keep in mind that everything you want to do AFTER you get the users from the database must
-    //   happen inside of this callback for it to be synchronous 
-    // Make sure you handle the case when there is an error, as well as the case when there is no error
-    console.log(users);
-    res.render('index', {users})
+  Mongoose.find({}, function(err, mongooses){
+    res.render('index', {mongooses})
   })
 })
 
-// Add User Request 
-app.post('/users', function(req, res) {
-  console.log("POST DATA", req.body);
-  // This is where we would add the user from req.body to the database.
-  // res.redirect('/');
+app.get('/mongooses/new', function(req, res) {  
+  res.render('new')
+})
 
-  var user = new User({name: req.body.name, age: req.body.age});
-  // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
-  user.save(function(err) {
-    // if there is an error console.log that something went wrong!
+app.get('/mongooses/:id', function(req, res) {
+  console.log("ID!", req.params.id);
+  Mongoose.findOne({_id: req.params.id}, function(err, mongoose){
+    res.render('detail', {mongoose})
+  })
+})
+
+
+
+
+
+app.post('/mongooses', function(req, res) {
+  console.log("POST DATA", req.body);
+
+  var mongoose = new Mongoose({
+    name: req.body.name, age: req.body.age
+  });
+  mongoose.save(function(err) {
     if(err) {
       console.log('something went wrong');
-    } else { // else console.log that we did well and then redirect to the root route
-      console.log('successfully added a user!');
+    } else { 
+      console.log('successfully added a mongoose!');
       res.redirect('/');
     }
   })
 })
+
+app.get('/mongooses/edit/:id', function(req, res) {  
+  Mongoose.findOne({}, function(err, mongoose){
+    res.render('edit', {mongooses})
+  })
+})
+
+app.post('/mongooses/destroy/:id', function(req, res) {
+  console.log("POST DATA", req.body);
+  Mongoose.findOne({}, function(err, mongoose){
+    var mongoose = new Mongoose({
+      name: req.body.name, age: req.body.age
+    });
+    mongoose.remove(function(err) {
+      if(err) {
+        console.log('something went wrong');
+      } else { 
+        console.log('successfully added a mongoose!');
+        res.redirect('/');
+      }
+    })
+  })
+})
+
+app.post('/mongooses/:id', function(req, res) {
+  console.log("POST DATA", req.body);
+  Mongoose.findOne({}, function(err, mongoose){
+    var mongoose = new Mongoose({
+      name: req.body.name, age: req.body.age
+    });
+    mongoose.save(function(err) {
+      if(err) {
+        console.log('something went wrong');
+      } else { 
+        console.log('successfully added a mongoose!');
+        res.redirect('/');
+      }
+    })
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
     console.log("listening on port 8000");
